7483e37bd646ae1b0bc3bc2d76ae408358de1b17
mitoop <me@mitoop.com>
2023-12-29 15:32:58 +0800

    adjust

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -233,83 +233,85 @@ $finder = Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
+class LaravelPhpdocAlignmentFixer implements FixerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return 'Laravel/laravel_phpdoc_alignment';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isAnyTokenKindsFound([T_DOC_COMMENT]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isRisky(): bool
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fix(SplFileInfo $file, Tokens $tokens): void
+    {
+        for ($index = $tokens->count() - 1; $index > 0; $index--) {
+            if (! $tokens[$index]->isGivenKind([\T_DOC_COMMENT])) {
+                continue;
+            }
+
+            $newContent = preg_replace_callback(
+                '/(?P<tag>@param)\s+(?P<hint>(?:'.TypeExpression::REGEX_TYPES.')?)\s+(?P<var>(?:&|\.{3})?\$\S+)/ux',
+                fn ($matches) => $matches['tag'].'  '.$matches['hint'].'  '.$matches['var'],
+                $tokens[$index]->getContent()
+            );
+
+            if ($newContent == $tokens[$index]->getContent()) {
+                continue;
+            }
+
+            $tokens[$index] = new Token([T_DOC_COMMENT, $newContent]);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition('@param and type definition must be followed by two spaces.', []);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPriority(): int
+    {
+        return -42;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(SplFileInfo $file): bool
+    {
+        return true;
+    }
+}
+
 return (new Config())->setFinder($finder)
     ->setRules($rules)
     ->setRiskyAllowed(true)
     ->setUsingCache(true)
     ->registerCustomFixers([
-        new class() implements FixerInterface
-        {
-            /**
-             * {@inheritdoc}
-             */
-            public function getName(): string
-            {
-                return 'Laravel/laravel_phpdoc_alignment';
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function isCandidate(Tokens $tokens): bool
-            {
-                return $tokens->isAnyTokenKindsFound([T_DOC_COMMENT]);
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function isRisky(): bool
-            {
-                return false;
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function fix(SplFileInfo $file, Tokens $tokens): void
-            {
-                for ($index = $tokens->count() - 1; $index > 0; $index--) {
-                    if (! $tokens[$index]->isGivenKind([\T_DOC_COMMENT])) {
-                        continue;
-                    }
-
-                    $newContent = preg_replace_callback(
-                        '/(?P<tag>@param)\s+(?P<hint>(?:'.TypeExpression::REGEX_TYPES.')?)\s+(?P<var>(?:&|\.{3})?\$\S+)/ux',
-                        fn ($matches) => $matches['tag'].'  '.$matches['hint'].'  '.$matches['var'],
-                        $tokens[$index]->getContent()
-                    );
-
-                    if ($newContent == $tokens[$index]->getContent()) {
-                        continue;
-                    }
-
-                    $tokens[$index] = new Token([T_DOC_COMMENT, $newContent]);
-                }
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getDefinition(): FixerDefinitionInterface
-            {
-                return new FixerDefinition('@param and type definition must be followed by two spaces.', []);
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function getPriority(): int
-            {
-                return -42;
-            }
-
-            /**
-             * {@inheritdoc}
-             */
-            public function supports(SplFileInfo $file): bool
-            {
-                return true;
-            }
-        },
+       new LaravelPhpdocAlignmentFixer()
     ]);
